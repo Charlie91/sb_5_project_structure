dde149eb500010e19b5ada21d043e3df137473a4
Your Name <you@example.com>
2024-01-10 22:30:37 +0300

    add typescript

---
+++ src/ui/Card/index.tsx
@@ -0,0 +1,31 @@
+import { Button } from "../Button";
+import './styles.css';
+
+type TProps = {
+    img: string;
+    features: string[];
+    title: string;
+    description: string;
+    badges: string[];
+    onChooseRoom: (roomName: string) => void;
+};
+
+export const Card = ({ img, features, title, description, badges, onChooseRoom }: TProps) => {
+    const handleButtonClick = () => onChooseRoom(title);
+    return (
+        <div className='card'>
+            <img src={img} />
+            <ul className='features'>
+                {features.map(feature => (
+                    <li>{feature}</li>
+                ))}
+            </ul>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <div className='badges'>
+                {badges.map(badge => <div className='badge'>{badge}</div>)}
+            </div>
+            <Button onClick={handleButtonClick}>Выбрать</Button>
+        </div>
+    )
+}
